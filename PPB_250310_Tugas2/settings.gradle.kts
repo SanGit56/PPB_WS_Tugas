fd09557b078514b8b6cab1a460c268770835b70d
SanGit56 <radhiyanrmhhisan@gmail.com>
2025-03-24 12:33:18 +0700

    menambahkan tugas 2

---
+++ PPB_250310_Tugas2/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PPB D_Tugas 2_5025211166"
+include(":app")
